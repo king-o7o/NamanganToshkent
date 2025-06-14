d315b1fbd84033309465212b114a9befb5757609
king-o7o <97437498+king-o7o@users.noreply.github.com>
2025-06-14 11:30:38 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -158,8 +158,8 @@ def build_info_text(message: Message) -> str:
 # --- АДМИН-ПАНЕЛЬ ---
 async def manage_id_list(message: Message, command: str, list_name: str, add_msg: str, remove_msg: str, list_title: str):
     parts = message.text.split(maxsplit=1)
-    action = command.split('_')[0] 
-
+    action = "list" if command == "list" else "add" if "add" in command else "remove"
+    
     if action == "list":
         item_list = db.data.get(list_name, [])
         if not item_list: return await message.reply("Рўйхат бўш.")
